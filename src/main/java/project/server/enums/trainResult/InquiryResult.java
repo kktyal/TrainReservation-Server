aaf78f45dbf806833fa895e78dd8545e914387c5
kimseunghyun <tyal78@naver.com>
2023-06-17 17:03:20 +0900

    모든 return 방식 정리 enum 정리 , 코드 최적화 진행중

---
+++ src/main/java/project/server/enums/trainResult/InquiryResult.java
@@ -0,0 +1,10 @@
+package project.server.enums.trainResult;
+
+import project.server.enums.interfaces.IResult;
+
+public enum InquiryResult implements IResult {
+    NO_SEARCH_DATA,
+    API_ERROR
+    
+    
+}
